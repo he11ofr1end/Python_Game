66ba18939b0f8d031d85a2d16def420a44f07ab6
anonymous203030 <aram.simonyan.03@gmail.com>
2026-01-13 20:19:14 +0400

    player integration to game

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame as pg
 import os
 
 from map import Map
+from player import Player
 from src.settings import RES, FPS
 
 
@@ -14,16 +15,18 @@ class Game:
         # set screen resolution and clock
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
-
+        self.delta_time = 1
         # Generate new game
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)
+        self.player = Player(self)
 
     def update(self):
+        self.player.update()
         pg.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps():.1f}')
 
     def draw(self):
@@ -32,6 +35,7 @@ class Game:
 
         # Draw map from map.py file
         self.map.draw()
+        self.player.draw()
 
     def check_events(self):
         for event in pg.event.get():
